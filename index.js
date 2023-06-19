13fe56211812b098c511a33628c8f309fdd3ae3f
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-06-19 17:36:39 +0200

    added es6 classes

--- index.js
+++ index.js
@@ -1,27 +1,106 @@
-// Store Library
-let myLibrary = [];
+// Book Class
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-// Book Constructor
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+    info() {
+        let readStatus = this.read ? 'read.' : 'not read yet.';
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+    }
 }
 
-// Book Info Prototype
-Book.prototype.info = function () {
-    let readStatus = this.read ? 'read.' : 'not read yet.';
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-}
+class Library {
+    constructor() {
+        this.books = [];
+    }
 
-// Add Book Function
-function addBookToLibrary(myLibrary, book) {
-    myLibrary.push(book);
-}
+    addBook(book) {
+        this.books.push(book);
+    }
 
+    removeBook(book) {
+        const index = this.books.indexOf(book);
+        if (index > -1) {
+            this.books.splice(index, 1)
+        }
+    }
+
+    toggleReadStatus(book) {
+        book.read = !book.read;
+    }
+
+    showBooks() {
+        const container = document.createElement('div');
+        container.classList.add('container');
+        container.setAttribute('id', 'myContainer');
+
+        // Loop through books
+        for (let i = this.books.length - 1; i >= 0; i--) {
+            const book = this.books[i];
+            const bookList = document.createElement('div');
+            bookList.classList.add('book');
+
+            const titleDiv = document.createElement('div');
+            titleDiv.classList.add('title');
+            titleDiv.textContent = 'Title: ' + book.title;
+
+            const authorDiv = document.createElement('div');
+            authorDiv.classList.add('author');
+            authorDiv.textContent = 'Author: ' + book.author;
+
+            const pagesDiv = document.createElement('div');
+            pagesDiv.classList.add('pages');
+            pagesDiv.textContent = 'Pages: ' + book.pages;
+
+            const readDiv = document.createElement('div');
+            readDiv.classList.add('read');
+
+            const readLabel = document.createElement('label');
+            readLabel.textContent = 'Read: ';
+
+            const readCheckbox = document.createElement('input');
+            readCheckbox.type = 'checkbox';
+            readCheckbox.checked = book.read;
+
+            readCheckbox.addEventListener('change', () => {
+                this.toggleReadStatus(book);
+            });
+
+            readDiv.appendChild(readLabel);
+            readDiv.appendChild(readCheckbox);
+
+            const button = document.createElement('button');
+            button.classList.add('delete');
+            button.textContent = 'Remove';
+            button.addEventListener('click', () => {
+                this.removeBook(book);
+                this.showBooks();
+            });
+
+            bookList.appendChild(titleDiv);
+            bookList.appendChild(authorDiv);
+            bookList.appendChild(pagesDiv);
+            bookList.appendChild(readDiv);
+            bookList.appendChild(button);
+            container.appendChild(bookList);
+        }
+
+        const existingContainer = document.getElementById('myContainer');
+        if (existingContainer) {
+            existingContainer.replaceWith(container);
+        } else {
+            document.body.appendChild(container);
+        }
+    }
+}
 
 // Instantiate Objects
+const myLibrary = new Library();
+
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 const aGameOfThrones = new Book('A Game Of Thrones', 'George R. R. Martin', 835, false);
 const nineteenEightyFour = new Book('1987', 'George Orwell', 328, true);
@@ -31,111 +110,32 @@ const sapiens = new Book('Sapiens: A Brief History of Humankind', 'Yuval Noah Ha
 const theAlchemist = new Book('The Alchemist', 'Paulo Coelho', 208, true);
 const thinkingFastAndSlow = new Book('Thinking, Fast and Slow', 'Daniel Kahneman', 499, false);
 
+myLibrary.addBook(theHobbit);
+myLibrary.addBook(aGameOfThrones);
+myLibrary.addBook(nineteenEightyFour);
+myLibrary.addBook(deepWork);
+myLibrary.addBook(thePowerOfNow);
+myLibrary.addBook(sapiens);
+myLibrary.addBook(theAlchemist);
+myLibrary.addBook(thinkingFastAndSlow);
 
-// Add Books
-addBookToLibrary(myLibrary, theHobbit);
-addBookToLibrary(myLibrary, aGameOfThrones);
-addBookToLibrary(myLibrary, nineteenEightyFour);
-addBookToLibrary(myLibrary, deepWork);
-addBookToLibrary(myLibrary, thePowerOfNow);
-addBookToLibrary(myLibrary, sapiens);
-addBookToLibrary(myLibrary, theAlchemist);
-addBookToLibrary(myLibrary, thinkingFastAndSlow);
-
-// Create DOM Elements
-const container = document.createElement('div');
-container.classList.add('container');
-container.setAttribute('id', 'myContainer');
-document.body.appendChild(container);
-
-// Remove Book
-function removeBookFromLibrary(book) {
-    const index = myLibrary.indexOf(book);
-    if (index > -1) {
-        myLibrary.splice(index, 1);
-    }
-}
-
-// Toggle Read
-function toggleReadStatus(book) {
-    book.read = !book.read;
-}
-
-// Show Books
-function showBooks() {
-    const reversedLibrary = myLibrary.slice().reverse();
-    container.innerHTML = '';
-
-    // Loop Through Books
-    for (let i = 0; i < reversedLibrary.length; i++) {
-        const book = reversedLibrary[i];
-
-        const bookList = document.createElement('div');
-        bookList.classList.add('book');
-
-        const titleDiv = document.createElement('div');
-        titleDiv.classList.add('title');
-        titleDiv.textContent = 'Title: ' + book.title;
-
-        const authorDiv = document.createElement('div');
-        authorDiv.classList.add('author');
-        authorDiv.textContent = 'Author: ' + book.author;
-
-        const pagesDiv = document.createElement('div');
-        pagesDiv.classList.add('pages');
-        pagesDiv.textContent = 'Pages: ' + book.pages;
-
-        const readDiv = document.createElement('div');
-        readDiv.classList.add('read');
-
-        const readLabel = document.createElement('label');
-        readLabel.textContent = 'Read: ';
-
-        const readCheckbox = document.createElement('input');
-        readCheckbox.type = 'checkbox';
-        readCheckbox.checked = book.read;
-
-        readCheckbox.addEventListener('change', function () {
-            toggleReadStatus(book);
-        });
-
-        readDiv.appendChild(readLabel);
-        readDiv.appendChild(readCheckbox);
-
-        const button = document.createElement('button');
-        button.classList.add('delete');
-        button.textContent = 'Remove';
-        button.addEventListener('click', function () {
-            removeBookFromLibrary(book);
-            showBooks();
-        });
-
-        bookList.appendChild(titleDiv);
-        bookList.appendChild(authorDiv);
-        bookList.appendChild(pagesDiv);
-        bookList.appendChild(readDiv);
-        bookList.appendChild(button);
-        container.appendChild(bookList);
-    }
-}
-
-showBooks();
+myLibrary.showBooks();
 
 const showModalButton = document.getElementById('showModalButton');
 const modal = document.getElementById('modalForm');
 const close = document.querySelector('.close');
 
-showModalButton.addEventListener('click', function () {
+showModalButton.addEventListener('click', () => {
     modal.style.display = 'block';
 });
 
-close.addEventListener('click', function () {
+close.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
 const myForm = document.getElementById('myForm');
 
-myForm.addEventListener('submit', function (e) {
+myForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const title = document.getElementById('title').value;
@@ -145,10 +145,10 @@ myForm.addEventListener('submit', function (e) {
 
     const newBook = new Book(title, author, pages, read);
 
-    addBookToLibrary(myLibrary, newBook);
+    myLibrary.addBook(newBook);
 
     myForm.reset();
 
-    showBooks();
+    myLibrary.showBooks();
     modal.style.display = 'none';
 });
